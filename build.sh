2d7a702cfed69dabe0dbecaeb48b8291b6d12329
Joshua Smith <joshua.barton.smith12@gmail.com>
2022-05-13 19:59:59 -0400

    update to python3.9 layer

--- build.sh
+++ build.sh
@@ -3,17 +3,17 @@ PKG_DIR='build_output/python/'
 LIB_DIR='build_output/lib/'
 
 # set the docker image name here (optional)
-IMAGE_NAME='nonbeing/lambda-python38-mysqlclient'
+IMAGE_NAME='nonbeing/lambda-python39-mysqlclient'
 
 sudo rm -rf build_output
 mkdir -p ${PKG_DIR} && mkdir -p ${LIB_DIR}
 
 # build a docker image closely matching the AWS Lambda environment, with mysql-devel installed
-docker build -t ${IMAGE_NAME} .
+podman build -t ${IMAGE_NAME} .
 
 if [ $? -eq 0 ]; then
     # actually build the layer zip now:
-    docker run --rm -v $(pwd):/foo -w /foo ${IMAGE_NAME} /foo/pip_and_copy.sh ${PKG_DIR} ${LIB_DIR}
+    podman run --rm -v $(pwd):/foo -w /foo ${IMAGE_NAME} /foo/pip_and_copy.sh ${PKG_DIR} ${LIB_DIR}
     cd build_output
     zip -r layer.zip .
 else
